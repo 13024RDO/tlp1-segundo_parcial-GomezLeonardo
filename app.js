aaf800e05ab05d3d32d4f6365a41666bf78e86c6
Leonardo <leoonardoogzz@gmail.com>
2024-06-27 15:14:14 -0600

    Se agrego el mostrar y el subir libros

--- app.js
+++ app.js
@@ -6,9 +6,49 @@ const app = express();
 app.use(express.text());
 app.use(express.json());
 
+
+
+
+
 app.get("/", (req,res)=>{
     res.send("Página de Inicio");
 });
+
+
+app.get('/books', (req, res) => {
+    res.json(db);
+});
+
+
+app.get('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    const book = db.find(b => b.id === bookId);
+    
+    if (!book) {
+        return res.status(404).json({ error: 'El libro no existe' });
+    }
+    
+    res.json(book);
+});
+
+
+app.post('/books', (req, res) => {
+    const { title, author, year } = req.body;
+    
+    if (!title || !author || !year) {
+        return res.status(400).json({ error: 'Todos los parametros son requeridos' });
+    }
+    
+    const existingBook = books.find(b => b.title === title && b.author === author && b.year === year);
+    if (existingBook) {
+        return res.status(400).json({ error: 'El libro ya exite' });
+    }
+    
+    const newBook = { id: nextId++, title, author, year };
+    books.push(newBook);
+    res.status(201).json(newBook);
+});
+
 
 
 app.listen(3000, () => {
--- db.js
+++ db.js
@@ -1,9 +1,10 @@
-db = [
+const db = [
     {
         id:1,
         title:"Don quijote",
         author:"Jose",
         year:23
     }
-]
+];
+
 module.exports = db;
