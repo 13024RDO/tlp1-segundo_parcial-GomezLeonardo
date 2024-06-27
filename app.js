871c24da059820912edc6ee02f049172a41b0289
Leonardo <leoonardoogzz@gmail.com>
2024-06-27 14:47:22 -0600

    Se creo la base de datos

---
+++ app.js
@@ -0,0 +1,6 @@
+const app  = require('express');
+const db = require('/db');
+
+app.listen(3000, ()=>{
+ console.log('Servidor Funcionando');
+});
---
+++ db.js
@@ -0,0 +1,9 @@
+db = [
+    {
+        id:1,
+        title:"Don quijote",
+        author:"Jose",
+        year:23
+    }
+]
+module.exports = db;
